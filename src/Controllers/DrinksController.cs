100d41a2e79a35531503e512209ea8396e2e7a7c
Jeremy Dunn <kin3tics@gmail.com>
2017-04-16 11:32:36 -0400

    Initial Commit

---
+++ src/Controllers/DrinksController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KegbotDotNetCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class DrinksController : Controller
+    {
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+    }
+}
