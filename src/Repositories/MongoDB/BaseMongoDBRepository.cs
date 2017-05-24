13f1d176bab7ac583d4c1c028ae22c115fab25ed
Jeremy Dunn <kin3tics@gmail.com>
2017-05-24 15:42:48 -0400

    modifying mongodb connection

--- src/Repositories/MongoDB/BaseMongoDBRepository.cs
+++ src/Repositories/MongoDB/BaseMongoDBRepository.cs
@@ -1,3 +1,4 @@
+using System.Security.Authentication;
 using MongoDB.Driver;
 
 namespace KegbotDotNetCore.API.Repositories.MongoDB
@@ -8,7 +9,10 @@ namespace KegbotDotNetCore.API.Repositories.MongoDB
         protected IMongoDatabase _db;
         public BaseMongoDBRepository() 
         {
-            _client = new MongoClient();
+            string connectionString = @"mongodb://kegbot-dotnetcore:vCd3SimxJl5bI6r0YBPYFnYa7OSrEFWsExs06H890I2SEXIVs0Ug3fCj3iaMdzcBNY7trJ2q2awdjOmERmuo0Q==@kegbot-dotnetcore.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            _client = new MongoClient(settings);
             _db = _client.GetDatabase("keggy");
         }
     }
